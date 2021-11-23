5aabac2a36b29511cbd89053960b8215ab993ebe
Константин Константин <kotkat.lv@gmail.com>
2021-11-23 17:05:20 +0300

    .

--- 1480.RunningSumOf1dArray/Solution.cs
+++ 1480.RunningSumOf1dArray/Solution.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+public static class Solution
+{
+    private static int _startValue;
+
+    public static void Main()
+    {
+        Console.WriteLine(FinalValueAfterOperations(new string[] { "++X", "++X", "X++" }));
+        Console.ReadKey();
+    }
+
+    public static int FinalValueAfterOperations(string[] operations)
+    {
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case "++X": ++_startValue; break;
+                case "--X": --_startValue; break;
+                case "X++": _startValue++; break;
+                case "X--": _startValue--; break;
+            }
+        }
+        return _startValue;
+    }
+}
