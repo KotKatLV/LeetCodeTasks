f5627c7d9e97d518f615bf14b8b6a2087b0095dd
Константин Константин <kotkat.lv@gmail.com>
2021-11-23 18:45:00 +0300

    .

--- 1480.RunningSumOf1dArray/Solution.cs
+++ 1480.RunningSumOf1dArray/Solution.cs
@@ -2,8 +2,16 @@
 {
     public static void Main()
     {
-       
+        Console.WriteLine(RunningSum(new int[] { 1, 2, 3, 4 }));
         Console.ReadKey();
     }
 
+    public static int[] RunningSum(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
 }
