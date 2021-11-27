43053423b6602d0bc0458c10f81e4cd295405996
Константин Константин <kotkat.lv@gmail.com>
2021-11-27 15:23:53 +0300

    .

---
+++ CamelCase/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+class Result
+{
+    public static int camelcase(string s)
+    {
+        List<string> list = new List<string>();
+        Regex regex = new Regex(@"((?<=\p{Ll})\p{Lu}|\p{Lu}(?=\p{Ll}))");
+        foreach (Match match in regex.Matches(s))
+        {
+            list.Add(match.Value);
+        }
+        return list.Count + 1;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+        int result = Result.camelcase(s);
+
+        textWriter.WriteLine(result);
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
