43312f49d20ab3d4be2c57be9b01b471912f9425
Константин Константин <kotkat.lv@gmail.com>
2021-11-27 16:14:39 +0300

    .

--- CamelCase/Solution.cs
+++ CamelCase/Solution.cs
@@ -2,29 +2,49 @@
 
 class Result
 {
-    public static int camelcase(string s)
+    private static readonly string _numbers = "0123456789";
+    private static readonly string _lower_case = "abcdefghijklmnopqrstuvwxyz";
+    private static readonly string _upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static readonly string _special_characters = "!@#$%^&*()-+";
+    private static readonly int _minPassLenght = 6;
+
+    public static int minimumNumber(int passwordLength, string password)
     {
-        List<string> list = new List<string>();
-        Regex regex = new Regex(@"((?<=\p{Ll})\p{Lu}|\p{Lu}(?=\p{Ll}))");
-        foreach (Match match in regex.Matches(s))
+        int result = 0;
+        if (password.IndexOfAny(_numbers.ToCharArray()) == -1)
         {
-            list.Add(match.Value);
+            ++result;
         }
-        return list.Count + 1;
+
+        if (password.IndexOfAny(_lower_case.ToCharArray()) == -1)
+        {
+            ++result;
+        }
+
+        if (password.IndexOfAny(_upper_case.ToCharArray()) == -1)
+        {
+            ++result;
+        }
+
+        if (password.IndexOfAny(_special_characters.ToCharArray()) == -1)
+        {
+            ++result;
+        }
+
+        if (result < _minPassLenght && passwordLength < _minPassLenght && result + passwordLength < _minPassLenght)
+        {
+            result += _minPassLenght - result - passwordLength;
+        }
+
+        return result;
     }
 }
 
 class Solution
 {
     public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        string s = Console.ReadLine();
-        int result = Result.camelcase(s);
-
-        textWriter.WriteLine(result);
-        textWriter.Flush();
-        textWriter.Close();
+    { 
+        Console.WriteLine(Result.minimumNumber(4, "4700"));
+        Console.ReadLine();
     }
 }
