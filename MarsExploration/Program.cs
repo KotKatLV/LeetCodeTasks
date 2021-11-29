6535b24b46a2e04980068d9830082f338f719e0a
Константин Константин <kotkat.lv@gmail.com>
2021-11-29 14:58:35 +0300

    .

---
+++ MarsExploration/Program.cs
@@ -0,0 +1,42 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(string s)
+    {
+        string sos = "SOS";
+        int result = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != sos[i % 3])
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.marsExploration(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
