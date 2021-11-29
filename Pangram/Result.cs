31518d1e0ad5fb8a70c9de56b9dc43b037cc68a0
Константин Константин <kotkat.lv@gmail.com>
2021-11-29 11:49:46 +0300

    .

---
+++ Pangram/Result.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    private static readonly int _countLettersInLatinAlphabet = 26;
+    public static string pangrams(string s) => Regex.Replace(s, @"\s+", "").ToLower().ToCharArray().Distinct().Count() == _countLettersInLatinAlphabet ? "pangram" : "not pangram";
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
